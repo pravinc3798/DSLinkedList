e8dff6a5c8fcbd6763e5e116ffd82b136da1d393
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-22 16:02:52 +0530

    Added LinkedListYT class

---
+++ LinkedListYT.cs
@@ -0,0 +1,199 @@
+﻿namespace LinkedListYT
+{
+    public class LinkedListYT
+    {
+        private class Node
+        {
+            public int Value;
+            public Node Next;
+
+            public Node(int value)
+            {
+                Value = value;
+            }
+        }
+
+        private Node Head;
+        private Node Tail;
+        int Size;
+
+        public int Length()
+        {
+            return Size;
+        }
+
+        public int Last()
+        {
+            return Tail.Value;
+        }
+
+        public void Display()
+        {
+            var temp = Head;
+
+            while (temp != null)
+            {
+                Console.Write("{0} -> ", temp.Value);
+                temp = temp.Next;
+            }
+            Console.WriteLine("END");
+        }
+
+        public void GetItemAt(int index)
+        {
+            var temp = Find(index + 1);
+            Console.WriteLine(temp.Value);
+        }
+
+        public int GetIndexOf(int value)
+        {
+            var count = 0;
+            var temp = Head;
+            while (temp != null)
+            {
+                if (temp.Value == value)
+                {
+                    return count;
+                }
+                temp = temp.Next;
+                count++;
+            }
+            return -2;
+        }
+
+        public void AddLast(int item)
+        {
+            var newNode = new Node(item);
+            if (Head == null)
+                Head = Tail = newNode;
+            else
+            {
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
+            Size += 1;
+        }
+
+        public void AddFirst(int item)
+        {
+            var newNode = new Node(item);
+            if (Head == null)
+                Head = Tail = newNode;
+            else
+            {
+                newNode.Next = Head;
+                Head = newNode;
+            }
+            Size += 1;
+        }
+
+        public void AddAtIndex(int item, int index)
+        {
+            if (index < 0 || index > Size)
+                Console.WriteLine("Index provided is out of range.");
+
+            else if (index == 0)
+                AddFirst(item);
+
+            else if (index == Size)
+                AddLast(item);
+
+            else
+            {
+                var temp = Head;
+                for (int i = 1; i < index; i++)
+                {
+                    temp = temp.Next;
+                }
+
+                var newNode = new Node(item);
+                newNode.Next = temp.Next;
+                temp.Next = newNode;
+                Size++;
+            }
+        }
+
+        public void AddAfter(int valueAdd, int afterValue)
+        {
+            var index = GetIndexOf(afterValue);
+            AddAtIndex(valueAdd, index+1);
+        }
+
+        public void DeleteFirst()
+        {
+            if (Head == Tail)
+                Head = Tail = null;
+            else
+            {
+                var temp = Head.Next;
+                Head = temp;
+                Size--;
+            }
+        }
+
+        private Node Find(int index)
+        {
+            if (index < 1 || index >= Size)
+                throw new ArgumentOutOfRangeException();
+            var temp = Head;
+            for (int i = 1; i < index; i++)
+                temp = temp.Next;
+            return temp;
+        }
+
+        public void DeleteLast()
+        {
+            if (Head == Tail)
+                Head = Tail = null;
+            else
+            {
+                var temp = Find(Size - 1);
+                Tail = temp;
+                Tail.Next = null;
+                Size--;
+            }
+        }
+
+        public void DeleteAtIndex(int index)
+        {
+            if (index == 0)
+                DeleteFirst();
+            else if (index == Size - 1)
+                DeleteLast();
+            else
+            {
+                var temp = Find(index);
+                temp.Next = temp.Next.Next;
+            }
+        }
+
+        public void DeleteValue(int value)
+        {
+            var index = GetIndexOf(value);
+            DeleteAtIndex(index);
+        }
+
+        public void SortList()
+        {
+            var previous = Head;
+            var current = Head.Next;
+
+            while (current != null)
+            {
+                if (current.Value < previous.Value)
+                {
+                    previous.Next = current.Next;
+                    current.Next = Head;
+                    Head = current;
+
+                    current = previous;
+                }
+                else
+                {
+                    previous = current;
+                    current = current.Next;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace LinkedListYT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("\n-------UC2 : Add elements from Head--------\n");
+            var UC2 = new LinkedListYT();
+            UC2.AddFirst(70);
+            UC2.Display();
+            UC2.AddFirst(30);
+            UC2.Display();
+            UC2.AddFirst(56);
+            UC2.Display();
+
+            Console.WriteLine("\n-------UC3 : Add elements to the Last--------\n");
+            var UC3 = new LinkedListYT();
+            UC3.AddLast(56);
+            UC3.Display();
+            UC3.AddLast(30);
+            UC3.Display();
+            UC3.AddLast(70);
+            UC3.Display();
+
+            Console.WriteLine("\n-------UC4 : Add 30 in Between 56 and 70--------\n");
+            var UC4 = new LinkedListYT();
+            UC4.AddFirst(56);
+            UC4.AddLast(70);
+            UC4.Display();
+            UC4.AddAtIndex(30,1);
+            UC4.Display();
+
+            Console.WriteLine("\n-------UC5 : Delete Last Element--------\n");
+            UC4.Display();
+            UC4.DeleteLast();
+            UC4.Display();
+
+            Console.WriteLine("\n-------UC6 : Delete First Element--------\n");
+            UC4.AddLast(70);
+            UC4.Display();
+            UC4.DeleteFirst();
+            UC4.Display();
+
+            Console.WriteLine("\n-------UC7 : Find Index of node with value 30--------\n");
+            UC4.AddFirst(56);
+            Console.WriteLine("Index : {0}",UC4.GetIndexOf(30));
+
+            Console.WriteLine("\n-------UC8 : Insert 40 after 30--------\n");
+            UC4.AddAfter(40,30);
+            UC4.Display();
+
+            Console.WriteLine("\n-------UC9 : Delete 40 from The linkedList--------\n");
+            UC4.Display();
+            UC4.DeleteValue(40);
+            UC4.Display();
+
+            Console.WriteLine("\n-------UC10 : Get the Length of Linked List--------\n");
+            Console.WriteLine("Length : {0}",UC4.Length());
+
+            Console.WriteLine("\n-------UC11 : Sort Linked List--------\n");
+            Console.WriteLine("Initial List");
+            UC4.AddLast(20);
+            UC4.AddLast(10);
+            UC4.AddFirst(95);
+            UC4.Display();
+            UC4.SortList();
+            Console.WriteLine("Sorted List");
+            UC4.Display();
+
+            Console.WriteLine("\n-------xxxxx--------\n");
+
+
+            var stack = new StackYT();
+
+            Console.WriteLine("\n-------Stack UC1 : Create stack of 56 30 70--------\n");
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+
+            Console.WriteLine("\n-------Stack UC2 : Peek and Pop from Stack--------\n");
+            Console.WriteLine("Peak : {0}",stack.Peak());
+            Console.WriteLine("Pop : {0}",stack.Pop());
+            Console.WriteLine("Peak again : {0}",stack.Peak());
+
+            Console.WriteLine("\n-------xxxxxxxxx--------\n");
+
+
+            var queue = new QueueYT();
+
+            Console.WriteLine("\n-------Queue UC1 : Create queue of 56 30 70--------\n");
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            Console.WriteLine("\n-------Queue UC2 : Ability to Dequeue--------\n");
+            Console.WriteLine("Size of Queue Before Dequeue : {0}",queue.Size());
+            Console.WriteLine("Dequeued Item : {0}",queue.Dequeue());
+            Console.WriteLine("Size of Queue After Dequeue : {0}", queue.Size());
+        }
+    }
+}
