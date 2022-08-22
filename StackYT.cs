1a51946e51ebd2d80bffe2851b44ba0dcb08b9ad
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-22 16:03:02 +0530

    Added StackYT class

---
+++ StackYT.cs
@@ -0,0 +1,32 @@
+﻿namespace LinkedListYT
+{
+    public class StackYT
+    {
+        LinkedListYT stack = new LinkedListYT();
+        public void Push(int item)
+        {
+            stack.AddLast(item);
+        }
+        public int Pop()
+        {
+            var item = stack.Last();
+            stack.DeleteLast();
+            return item;
+        }
+        public int Peak()
+        {
+            var item = stack.Last();
+            return item;
+        }
+        public bool isEmpty()
+        {
+            if (stack.Length() == 0)
+                return true;
+            return false;
+        }
+        public int size()
+        {
+            return stack.Length();
+        }
+    }
+}
