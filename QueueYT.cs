2930583036f8ffd98930b176bb623306ad8436f8
pravinc3798 <pravin.choudhary88@gmail.com>
2022-08-22 16:02:57 +0530

    Added QueueYT class

---
+++ QueueYT.cs
@@ -0,0 +1,32 @@
+﻿namespace LinkedListYT
+{
+    public class QueueYT
+    {
+        LinkedListYT queue = new LinkedListYT();
+        
+        public void Enqueue(int item)
+        {
+            queue.AddLast(item);
+        }
+
+        public int Dequeue()
+        {
+            var item = queue.Last();
+            queue.DeleteFirst();
+            return item;
+        }
+
+        public bool isEmpty()
+        {
+            if (queue.Length() == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public int Size()
+        {
+            return queue.Length();
+        }
+    }
+}
